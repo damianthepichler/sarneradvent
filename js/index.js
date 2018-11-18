dc7af950d970af8aef041598c4604cd2a21f7200
damianthepichler <damian.pichler@gmail.com>
2018-11-18 17:11:39 +0100

    Add animation-plugin and testinf-files

---
+++ js/index.js
@@ -0,0 +1,51 @@
+
+//var dateNow  = moment();
+moment.locale('de');
+var dateNow  = 20181203;
+var dateFrom = 20181201;
+var dateTo   = 20181224;
+var $pageTemplate   = $('.js-page-template');
+var $pages          = $('.js-pages');
+var pages = [];
+
+var filesBasePath = 'files/';
+var filesEnding   = '.mp3';
+var files = {
+    1 : 'test1',
+    2 : 'test2',
+    3 : 'test3',
+    4 : 'test4',
+    5 : 'test5',
+};
+
+$(function() {
+
+  if (dateNow >= dateFrom) {
+    var iterationNumber = 1;
+
+    for (var thisDateString = dateFrom; thisDateString <= dateNow; thisDateString++) {
+        var thisPageDate = moment(thisDateString, 'YYYYMMDD');
+        var $thisPageObject = $pageTemplate.clone();
+        $thisPageObject.removeClass('hide-me');
+        $thisPageObject.addClass('page-' + thisDateString);
+        $thisPageObject.find('.js-audio-source').attr('src', filesBasePath + files[iterationNumber] + filesEnding);
+        $thisPageObject.find('.js-week-day').html(thisPageDate.format('dddd') + ',');
+        $thisPageObject.find('.js-day').html(thisPageDate.format('D'));
+        $thisPageObject.find('.js-month').html(thisPageDate.format('MMMM'));
+
+        pages.push({content : $thisPageObject.prop('outerHTML')});
+        iterationNumber++;
+    }
+
+
+    var slider = new iSlider(document.getElementById('js-pages'), pages, {
+        // isLooping: 1,
+        isOverspread: 1,
+        // isAutoplay: 1,
+        animateTime: 800,
+        animateType: 'flow',
+        isVertical: true
+    });
+  }
+
+});
