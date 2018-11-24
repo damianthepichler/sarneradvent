b2dd382da5f818bdd9a27302a30a6fe4e725cb90
damianthepichler <damian.pichler@gmail.com>
2018-11-24 15:21:48 +0100

    Add new style, font and custom audio-controllers

--- js/index.js
+++ js/index.js
@@ -26,17 +26,32 @@ $(function() {
     for (var thisDateString = dateFrom; thisDateString <= dateNow; thisDateString++) {
         iterationNumber++;
         var thisPageDate = moment(thisDateString, 'YYYYMMDD');
-        var $thisPageObject = $pageTemplate.clone();
+        var $thisPageObject  = $pageTemplate.clone();
+        // var thisNativePlayer = $thisPageObject.find('.js-native-player');
         $thisPageObject.removeClass('hide-me');
         $thisPageObject.addClass('page-' + thisDateString);
         $thisPageObject.find('.js-audio-source').attr('src', filesBasePath + files[iterationNumber] + filesEnding);
         $thisPageObject.find('.js-week-day').html(thisPageDate.format('dddd') + ',');
         $thisPageObject.find('.js-day').html(thisPageDate.format('D'));
         $thisPageObject.find('.js-month').html(thisPageDate.format('MMMM'));
-
         pages.push({content : $thisPageObject.prop('outerHTML')});
     }
 
+    var $buttonPlayPause = $('.js-button-play-pause');
+
+    $(document).on('click', $buttonPlayPause, function(e){
+        var $this = $(e.target);
+        if (!$this.hasClass('js-button-play-pause')) {
+            $this = $this.closest('.js-button-play-pause');
+        }
+        $this.toggleClass('button--play-pause--play button--play-pause--pause');
+
+        var thisPlayer = $this.closest('.js-page-content').find('.js-native-player')[0];
+        thisPlayer[thisPlayer.paused ? 'play' : 'pause']();
+        $(thisPlayer).off('ended').on('ended', function(){
+            $this.toggleClass('button--play-pause--play button--play-pause--pause');
+        });
+    });
 
     var slider = new iSlider(document.getElementById('js-pages'), pages, {
         initIndex : (iterationNumber - 1),
